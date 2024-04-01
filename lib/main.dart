f044a5e0d27ac75461fa00281d18ca50b5c0865b
AHMED3ly4 <ahmed.3lyyyy4@gmail.com>
2024-04-01 17:23:06 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MoviesApp());
+}
+
+class MoviesApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
